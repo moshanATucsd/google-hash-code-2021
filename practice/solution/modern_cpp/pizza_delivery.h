dd372ce790f7b68ed1a644d2dc393ab3ecc50c25
Sholmes <moshan@ucsd.edu>
2021-01-09 20:08:05 -0800

    add code

--- practice/solution/modern_cpp/pizza_delivery.h
+++ practice/solution/modern_cpp/pizza_delivery.h
@@ -1,4 +1,13 @@
 #ifndef PIZZA_DELIVERY_H
 #define PIZZA_DELIVERY_H
 
+class PizzaDelivery
+{
+public:
+    // constructor 
+    PizzaDelivery();
+    // default destructor 
+    ~PizzaDelivery() = default; 
+}
+
 #endif //PIZZA_DELIVERY_H 
